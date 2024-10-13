da9cc2c31234ada03d4ea996f27d621b7942a72b
Pikri <pikri@dbo.id>
2024-10-13 13:45:37 +0700

    create unittest for auth usecases 1/3

---
+++ app/usecases/auth_usecases_test.go
@@ -0,0 +1,60 @@
+package usecases
+
+import (
+	"context"
+	"github.com/golang/mock/gomock"
+	"github.com/jackc/pgx/v5"
+	"github.com/stretchr/testify/assert"
+	"oauth2/app/dto"
+	"oauth2/app/global/helper"
+	mocksMongo "oauth2/app/mock/repositories/mongo_repo"
+	mocksSql "oauth2/app/mock/repositories/sql_repo"
+	"oauth2/app/models"
+
+	"testing"
+)
+
+func TestValidateUser(t *testing.T) {
+	goMockController := gomock.NewController(t)
+	mockAuthRepo := mocksSql.NewMockIAuthRepository(goMockController)
+	mockAccessRepo := mocksMongo.NewMockIAccessTokenSessionsRepository(goMockController)
+	mockRefreshRepo := mocksMongo.NewMockIRefreshTokenSessionsRepository(goMockController)
+	useCase := NewAuthUseCase(mockAuthRepo, mockAccessRepo, mockRefreshRepo)
+
+	t.Run("+: admin", func(t *testing.T) {
+		returnValue := &models.Users{
+			Id:           1,
+			Username:     "admin",
+			Email:        "admin@live.com",
+			PasswordHash: "$2a$04$ZP1.DVAdR677eHTBUpDzE.0hHnp31JcyRK/eMF9Z7Y.iOWJkE/JNi",
+			RoleID:       1,
+			CreatedAt:    nil,
+			UpdatedAt:    nil,
+		}
+		mockAuthRepo.EXPECT().GetUserByUsernameOrEmail("admin", "", context.Background()).Return(returnValue, nil)
+		request := &dto.LoginRequest{
+			Username: "admin",
+			Email:    "",
+			Password: "admin",
+		}
+		response, errLog := useCase.ValidateUser(request, context.Background())
+		assert.Nil(t, errLog)
+		assert.NotNil(t, response)
+		assert.Equal(t, returnValue, response)
+	})
+
+	t.Run("-: does not exist user", func(t *testing.T) {
+		mockAuthRepo.EXPECT().GetUserByUsernameOrEmail("userNotExist", "", context.Background()).Return(nil, helper.WriteLog(pgx.ErrNoRows, 404, "please enter valid username"))
+		request := &dto.LoginRequest{
+			Username: "userNotExist",
+			Email:    "",
+			Password: "userNotExist",
+		}
+		response, errLog := useCase.ValidateUser(request, context.Background())
+
+		expectedError := helper.WriteLog(pgx.ErrNoRows, 404, "please enter valid username")
+		assert.Nil(t, response)
+		assert.NotNil(t, errLog)
+		assert.Equal(t, expectedError, errLog)
+	})
+}
--- go.mod
+++ go.mod
@@ -24,6 +24,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gofiber/utils/v2 v2.0.0-beta.6 // indirect
+	github.com/golang/mock v1.6.0 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
@@ -33,6 +34,7 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.55.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
